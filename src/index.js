93900a9baa816e43fd7e4223d3310c4db40e4954
Oxana Khazhieva <oxana.khazhieva@gmail.com>
2020-10-06 17:14:52 +0300

    init firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import * as firebase from "firebase";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9JAOcrsM4B_M2ya44yb2gv97Rv9Kt-zg",
+  authDomain: "weekmenu-715a1.firebaseapp.com",
+  databaseURL: "https://weekmenu-715a1.firebaseio.com",
+  projectId: "weekmenu-715a1",
+  storageBucket: "weekmenu-715a1.appspot.com",
+  messagingSenderId: "642275803469",
+  appId: "1:642275803469:web:13394dd5374eca8d6db704",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
